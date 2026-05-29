26df6b0dc1ccd7651ebd760791caaba4992a5eab
shesolamillo <sheilamaesolamillo@gmail.com>
2026-05-30 03:45:14 +0800

    addwd use api patch in order entity

--- src/Entity/Order.php
+++ src/Entity/Order.php
@@ -16,6 +16,7 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
+use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Delete;
 
 use Symfony\Component\Serializer\Attribute\Groups;
